5269503cffa9d3f8ae229edba5ff682f92b919a7
Huyzilla <huytranquang20042004a@gmail.com>
2025-12-14 17:56:11 +0700

    RSTP

---
+++ config/cameras.py
@@ -0,0 +1,6 @@
+CAMERAS = {
+    "east": "rtsp://localhost:8554/east",
+    "west": "rtsp://localhost:8554/west",
+    "north": "rtsp://localhost:8554/north",
+    "south": "rtsp://localhost:8554/south",
+}
