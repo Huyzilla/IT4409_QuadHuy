6628a265e8e1c81d0e6fb7e84796445dcc45777f
Huyzilla <huytranquang20042004a@gmail.com>
2025-12-16 21:03:52 +0700

    update

--- config/cameras.py
+++
@@ -1,6 +0,0 @@
-CAMERAS = {
-    "east": "rtsp://localhost:8554/east",
-    "west": "rtsp://localhost:8554/west",
-    "north": "rtsp://localhost:8554/north",
-    "south": "rtsp://localhost:8554/south",
-}
