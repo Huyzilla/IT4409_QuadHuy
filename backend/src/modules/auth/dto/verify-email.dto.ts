7d73874e684842072e99fb3a21b356162842bd95
Kevin_NguyenS <gr2922004@gmail.com>
2025-12-22 00:08:15 +0700

    fix verify-email.dto

--- backend/src/modules/auth/dto/verify-email.dto.ts
+++ backend/src/modules/auth/dto/verify-email.dto.ts
@@ -1,12 +1,24 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  IsOptional,
+} from 'class-validator';
 
 export class VerifyEmailDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  //  Thêm @IsOptional() để cho phép không gửi code (nếu dùng link)
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   @Length(6, 6)
-  code: string;
+  code?: string;
+
+  // Thêm trường token để Backend nhận diện được link xác thực
+  @IsString()
+  @IsOptional()
+  token?: string;
 }
